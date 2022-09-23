8caa27e5f3f45ea139ab33cb91ad5f29392f98ae
pbochynski <p.bochynski@gmail.com>
2022-09-23 15:34:03 +0200

    Stable bundle names

--- preact/preact.config.js
+++ preact/preact.config.js
@@ -1,4 +1,20 @@
-module.exports = function(config) {
+/**
+ * @param {import('preact-cli').Config} config - Original webpack config
+ * @param {import('preact-cli').Env} env - Current environment info
+ * @param {import('preact-cli').Helpers} helpers - Object with useful helpers for working with the webpack config
+ */
+
+export default (config, env, helpers) => {
   config.output.filename = '[name].js';
   config.output.chunkFilename = '[name].chunk.js';
+  const babelEsmPlugin = helpers.getPluginsByName(config, 'BabelEsmPlugin')[0];
+  const miniExtractPlugin = helpers.getPluginsByName(config, 'MiniCssExtractPlugin')[0];
+  if (babelEsmPlugin) {
+    babelEsmPlugin.plugin.options_.chunkFilename = '[name].chunk.esm.js';
+    babelEsmPlugin.plugin.options_.filename = '[name].esm.js';
+  }
+  if (miniExtractPlugin) {
+    miniExtractPlugin.plugin.options.chunkFilename = '[name].chunk.css';
+    miniExtractPlugin.plugin.options.filename = '[name].css';
+  }
 };
